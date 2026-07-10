f04202c2ff55773279efd533c5418fffac943f60
palaksingh2309 <palak2004psingh@gmail.com>
2026-07-10 16:10:35 +0530

    Changes

--- com.cognizant/src/main/java/com/cognizant/com/cognizant/com/cognizant/SpringLearnApplication.java
+++ com.cognizant/src/main/java/com/cognizant/com/cognizant/com/cognizant/SpringLearnApplication.java
@@ -1,12 +1,38 @@
-package com.cognizant.springlearn;
+package com.cognizant.com.cognizant.com.cognizant;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cognizant.Country;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
+
     public static void main(String[] args) {
+
         SpringApplication.run(SpringLearnApplication.class, args);
+
+        displayCountry();
+
     }
+
+    public static void displayCountry() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        LOGGER.debug("Country : {}", country);
+
+    }
+
 }
